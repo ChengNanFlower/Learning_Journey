e4bbe1e384e3172fa9c6a5ef29adb880296ddc5a
XiaoYou <xiaoyou@localhost.localdomain>
2024-12-13 20:33:39 +0800

    thing

--- CS50/Lecture4-Memory/cp.c
+++ CS50/Lecture4-Memory/cp.c
@@ -5,14 +5,14 @@ typedef uint8_t BYTE;
 
 int main(int argc,char *argv[])
 {
-    FILE *src = fopen(argv[1],"rb");
-    FILE *dst = fopen(argv[2],"wb");
+    FILE *src = fopen(argv[1],"rb"); //打开指定路径的文件以 只读模式（"rb"）访问，返回一个指向该文件的文件指针src。
+    FILE *dst = fopen(argv[2],"wb"); //打开或创建指定路径的文件以 写入模式（"wb"）访问，返回一个指向该文件的文件指针dst。
 
     BYTE b;
 
-    while (fread(&b,sizeof(b),1,src) != 0)
+    while (fread(&b,sizeof(b),1,src) != 0) //一个个字节读取源文件
     {
-        fwrite(&b,sizeof(b),1,dst);
+        fwrite(&b,sizeof(b),1,dst);// 依次写入到目标文件
     }
 
     fclose(dst);
