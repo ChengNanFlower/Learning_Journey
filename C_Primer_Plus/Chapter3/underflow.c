fa31b57b4ecf187ece8c035cad940d38ed041cf7
XiaoYou <xiaoyou@localhost.localdomain>
2024-12-17 12:04:35 +0800

    update underflow

--- C_Primer_Plus/Chapter3/underflow.c
+++ C_Primer_Plus/Chapter3/underflow.c
@@ -3,11 +3,11 @@
 #include <float.h> // 包含浮点数范围的宏定义
 
 int main(void) {
-    double tiny = 1.0; // 初始化为一个正常数值
+    double tiny = 1.0;
 
     // 逐步减小 tiny 的值，模拟下溢
     while (tiny > 0) {
-        tiny /= 10.0; // 每次将 tiny 减小 10 倍
+        tiny /= 10.0;
         printf("Current value: %.14e\n", tiny);
     }
 
